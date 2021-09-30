60c4c78baac9115e688690fe6e6890d48889e48f
Dominik Pilz <domipilz@gmx.net>
2021-09-30 20:35:08 +0200

    more nice stuff

--- orm-framework/lib/src/orm_models/orm_entity.dart
+++ orm-framework/lib/src/orm_models/orm_entity.dart
@@ -1,13 +1,17 @@
 import 'package:orm_framework/src/orm_metadata/entity_metadata.dart';
+import 'package:orm_framework/src/orm_metadata/ignore_metadata.dart';
+import 'package:orm_framework/src/orm_metadata/primary_key_metadata.dart';
 import 'package:orm_framework/src/orm_models/orm_field.dart';
 import 'dart:mirrors';
 import 'package:collection/collection.dart';
 
 class OrmEntity {
   OrmEntity(Type type){
-    final DeclarationMirror classDeclaration = reflectClass(type);
-    final ClassMirror entityAnnotationMirror = reflectClass(EntityMetadata);
-    final InstanceMirror? annotationInstsanceMirror = classDeclaration.metadata.firstWhereOrNull((d) => d.type == entityAnnotationMirror);
+    member = type;
+
+    final DeclarationMirror classDeclarationMirror = reflectClass(type);
+    final ClassMirror metadataClassMirror = reflectClass(EntityMetadata);
+    final InstanceMirror? annotationInstsanceMirror = classDeclarationMirror.metadata.firstWhereOrNull((d) => d.type == metadataClassMirror);
     
     String? tableNameOfEntity;
     // if annotationInstsanceMirror is null, the annotation is no present
@@ -16,8 +20,23 @@ class OrmEntity {
     }
 
     if (tableNameOfEntity == null) {
-      tableName = MirrorSystem.getName(classDeclaration.simpleName).toUpperCase();
+      tableName = MirrorSystem.getName(classDeclarationMirror.simpleName).toUpperCase();
     }
+
+    List<OrmField> fields = <OrmField>[];
+
+    final ClassMirror classClassMirror = reflectClass(type);
+    classClassMirror.declarations.forEach((_, value) {
+      
+      final ClassMirror ignoreMetadataClassMirror = reflectClass(IgnoreMetadata);
+      final bool isIgnored = classDeclarationMirror.metadata.firstWhereOrNull((d) => d.type == ignoreMetadataClassMirror) != null;
+
+      if(!value.isPrivate && !isIgnored){
+        OrmField field = (this);
+        final ClassMirror primaryKeyMetadataClassMirror = reflectClass(PrimaryKeyMetadata);
+        final bool isPrimary = classDeclarationMirror.metadata.firstWhereOrNull((d) => d.type == primaryKeyMetadataClassMirror) != null;
+      }
+    });
   }
 
   late Type member;
