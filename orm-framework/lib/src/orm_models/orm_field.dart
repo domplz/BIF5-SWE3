bf1fd4d1b2ac758bc46eed0505494322423f48ac
Dominik Pilz <domipilz@gmx.net>
2021-10-05 19:29:12 +0200

    implemented field stuff

--- orm-framework/lib/src/orm_models/orm_field.dart
+++ orm-framework/lib/src/orm_models/orm_field.dart
@@ -2,6 +2,8 @@ import 'dart:mirrors';
 
 import 'package:orm_framework/src/orm_models/orm_entity.dart';
 
+import '../../orm_framework.dart';
+
 class OrmField {
   OrmField(this.entity, this.member, this.type, this.columnName, this.columnType, this.isPrimaryKey, this.isForeignKey, this.isNullable);
 
@@ -13,4 +15,49 @@ class OrmField {
   bool isPrimaryKey;
   bool isForeignKey;
   bool isNullable;
+
+  toColumnType(Object value){
+    if(isForeignKey){
+      return Orm.getEntity(type).primaryKey.toColumnType(Orm.getEntity(type).primaryKey.getValue());
+    }
+    if(type == columnType){
+      return value;
+    }
+    if(value == bool){
+      if(columnType == int){
+        return (value as bool) ? 1 : 0;
+      }
+    }
+
+    return value;
+  }
+
+  toFieldType(Object value){
+    if(type == bool){
+      if(value is int){
+        return value != 0;
+      }
+    }
+    if(type == int){
+      return value as int;
+    }
+    
+    return value;
+  }
+  
+  Object getValue(){
+    if(member.runtimeType.toString() == "_VariableMirror"){
+      InstanceMirror instanceMirror = reflect(member);
+      return instanceMirror.getField(member.simpleName);
+    }
+    throw Exception("Other types than VariableMirrors are not supportet for getValue!");
+  }
+
+  void setValue(Object value){
+    if(member.runtimeType.toString() == "_VariableMirror"){
+      InstanceMirror instanceMirror = reflect(member);
+      instanceMirror.setField(member.simpleName, value);
+    }
+    throw Exception("Other types than VariableMirrors are not supportet for setValue!");
+  }
 }
