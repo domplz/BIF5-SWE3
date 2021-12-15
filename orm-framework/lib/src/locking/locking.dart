f0ecf2bc0a8acadec27e9c3b634220bf73c14c66
domplz <dominik.pilz@outlook.com>
2021-12-15 13:45:55 +0100

    comments

--- orm-framework/lib/src/locking/locking.dart
+++ orm-framework/lib/src/locking/locking.dart
@@ -1,5 +1,8 @@
+/// Abstract implementation of [Locking]
 abstract class Locking {
+  /// Locks the [object]
   void lock(Object object);
 
+  /// Releases the [object]
   void release(Object object);
 }
